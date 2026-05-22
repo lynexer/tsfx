3f2f3698b9b06b88d34bbb769b8a2cd5ae2bb76c
Kyle Graham <kylegraham05@gmail.com>
2026-05-22 09:06:15 -0700

    chore(nui): export new ui components

--- packages/nui/src/index.ts
+++ packages/nui/src/index.ts
@@ -1,4 +1,15 @@
+export { Alert, AlertAction, AlertDescription, AlertTitle } from '@/components/ui/alert';
+export { Badge, badgeVariants } from '@/components/ui/badge';
 export { Button, buttonVariants } from '@/components/ui/button';
+export {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from '@/components/ui/card';
 export type { ThemeContextValue } from '@/contexts/theme-context';
 export { ThemeContext } from '@/contexts/theme-context';
 export { useTheme } from '@/hooks/use-theme';
